c6b861733af31c07fadf886b47e84bafbde7bd16
dhanraj082004@gmail.com <dhanraj082004@gmail.com>
2026-01-02 23:11:34 +0530

    new js pratice code

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+/*variable
 // var name ="Hai Dhanaraj";
 // var mes = "welcome" +name+"college";
 
-let name ="Ramkumar";
+//let name ="Ramkumar";
 let mes ="Kongunadu "+name+" student"
+
+const name = "raj";
+
+console.log(name);
 alert(mes);
-console.log(mes);
+console.log(mes);*/
+
+
+/*
+// Datatypes
+let Name = "Raj";
+let age = 33;
+let isCorrect = true
+let Normal;
+let troble = null
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof isCorrect);
+console.log(typeof Normal);
+console.log(Normal);
+console.log(typeof troble);
+*/
+
+// Object
+let person = {
+    Name : "Thalapathy",
+    age : 22,
+    isCorrect : true,
+    lists : {
+        balls: "football",
+        Bats:"Cricket"
+    }
+}
+
+console.log(person.lists.Bats);
